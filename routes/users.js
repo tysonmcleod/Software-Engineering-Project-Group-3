0f2cb91176e954e3522330db1cec78bc1de2e261
evangelia <vagspax@gmail.com>
2019-11-13 22:20:34 +0100

    log all users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  User.find(function (err, users) {
+    // if (err) return
+    console.log(users)
+  });
 });
 
 module.exports = router;
