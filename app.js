f9685f2e761208090fde0ca6519d7a85a311ad00
Johan Isaksson <jisaksson94@gmail.com>
2019-11-13 15:15:36 +0100

    Added map structure and initiated index route

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
+var createError = require('http-errors');
 var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
+const port = 3000;
 
-app.get('/', function(req, res) {
-    res.send('Hello World');
-})
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-var server = app.listen(8081, function() {
-    var host = server.address().address
-    var port = server.address().port
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-    console.log("Example app listening at http://%s:%s", host, port)
-})
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.set('port', (process.env.PORT || 8000));
+
+app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port'));
+});
+
+module.exports = app;
