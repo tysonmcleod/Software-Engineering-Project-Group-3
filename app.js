fb9e23d7a3fb7fa78c200530d467751b0e21dc98
mikaelamch <Mikaela.Micho.2374@student.uu.se>
2019-11-16 14:40:33 +0100

    Log in process -- not complete

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 var createError = require('http-errors');
 var express = require('express');
+var session = require('express-session');
+var passport = require('passport');
+var expressValidator = require('express-validator');
+var LocalStrategy =  require('passport-local').Strategy;
+var mumlter = require('multer');
+var upload = mumlter({dest:'./uploads'});
+var flash = require('connect-flash');
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://localhost:27017/mydb';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -23,6 +38,44 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+app.use(session({secret:'secret',saveUninitialized:'true',resave:'true'}));
+
+//Handle sessions
+app.use(session({secret:'secret',
+  saveUninitialized:'true',
+  resave:'true'
+}));
+
+//Passport  -- authentication system
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//Validator
+/*app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));*/
+
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -42,7 +95,8 @@ app.use(function(err, req, res, next) {
 app.set('port', (process.env.PORT || 8000));
 
 app.listen(app.get('port'), function(){
-	console.log('Server started on port '+app.get('port'));
+  console.log('Server started on port '+app.get('port'));
 });
 
 module.exports = app;
+
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  currentMonth = new calendar.Calendar(1).monthdays2calendar(2019, 11);
-  res.render('index', { title: 'Express', testString: 'NOT SENT', month: currentMonth });
-});
-
-router.post('/', function(req, res, next) {
-  currentMonth = new calendar.Calendar(1).monthdays2calendar(2019, 11);
-  res.render('index', { title: 'Express', testString: 'SENT', month: currentMonth });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
