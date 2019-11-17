e834c2b4b7cdff1eda2b4bd3c86d672d93b7dfbc
tysonmcleod <tyson.mcleod.8851@student.uu.se>
2019-11-17 19:19:43 +0100

    User reg and layout start
    start

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// route files
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var usersRouter = require('./routes/users');
+
+// user reg and login
+var users = require('./routes/users');
+// user reg and login
 
 var app = express();
 const port = 3000;
@@ -27,7 +32,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', users);
+
+// app.use('/users', usersRouter);
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+
+// Bring in user model
+var User = require('../models/user');
+
+
+// register form
+
+router.get('/', function(req,res){
+  res.render('register');
+});
+
+
+/* GET users listing.
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+*/
 module.exports = router;
