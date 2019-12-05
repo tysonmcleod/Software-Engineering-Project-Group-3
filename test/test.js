be7b269d5ba67ff84c4cd9d1a716f2b8ec99dbd0
tysonmcleod <tyson.mcleod.8851@student.uu.se>
2019-12-05 16:28:19 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,10 +1,2 @@
 var expect = require('chai').expect;
 var request = require('request');
-
-
-it('Main page status', function(done) {
-    request('http://localhost:8000' , function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-    });
-});
